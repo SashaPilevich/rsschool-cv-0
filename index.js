bcb2952bdd6180b302b5c53026b6bf8ca0f137c1
Pilevich Sasha <pilevichsasha@gmail.com>
2022-12-15 15:24:42 +0300

    refactor: change structure of the projects

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
+let englishBlock = document.getElementById('english');
+let percent = document.querySelector('.percent');
+let loader = document.querySelector('.english-loader');
+let interval;
 let current = 0;
-let englishBlock = document.getElementById("english");
+
 let heightBlock = englishBlock.offsetHeight;
 let offsetBlock = offset(englishBlock).top;
 const animStart = 4;
 let animItemPoint = window.innerHeight - heightBlock / animStart;
 
-function offset(el) {
-  const rect = el.getBoundingClientRect();
-  const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
-  const scrollTop = window.scrollY || document.documentElement.scrollTop;
-  return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
-}
-let percent = document.querySelector(".percent");
-let loader = document.querySelector(".english-loader");
-let interval;
-
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   if (
     scrollY > offsetBlock - animItemPoint &&
     screenY < offsetBlock + heightBlock
@@ -24,6 +18,53 @@ window.addEventListener("scroll", () => {
   }
 });
 
+
+const btnTop = document.getElementById('scrollToTop');
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 500) {
+    btnTop.style.display = 'block';
+  } else {
+    btnTop.style.display = 'none';
+  }
+});
+
+btnTop.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
+
+let isOpen = false;
+const burger = document.querySelector('.burger-menu');
+const burgerLine = document.querySelector('.burger-line');
+const mobilenav = document.querySelector('.mobile-nav');
+const link = document.querySelector('.nav-list-mobile');
+
+burger.addEventListener('click', () => {
+  if (!isOpen) {
+    burger.classList.add('active');
+    burgerLine.classList.add('active');
+    mobilenav.classList.add('active');
+    isOpen = true;
+  } else {
+    burger.classList.remove('active');
+    burgerLine.classList.remove('active');
+    mobilenav.classList.remove('active');
+    isOpen = false;
+  }
+});
+
+link.addEventListener("click", () => {
+  burger.classList.remove("active");
+  burgerLine.classList.remove("active");
+  mobilenav.classList.remove("active");
+  isOpen = false;
+});
+
+function offset(el) {
+  const rect = el.getBoundingClientRect();
+  const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+}
 function increment() {
   if (current <= 78) {
     current = current + 1;
@@ -33,35 +74,3 @@ function increment() {
     clearInterval(interval);
   }
 }
-
-const banner = document.querySelector('.profile-wrapper')
-const title = document.querySelector('.profile-info')
-const img = document.querySelector('.profile-image')
-
-window.addEventListener('scroll',() => {
-  parallax(banner,5);
-})
-function parallax(elem,speed){
-  let scroll_value = window.scrollY;
-  if(scroll_value <= 300){
-    elem.style.transform = 'translateX('+(-scroll_value/speed)+'px)';
-  } 
-  else {
-    return;
-  }
-}
-
-const btnTop = document.getElementById('scrollToTop')
-window.addEventListener('scroll',() => {
-  if(window.scrollY >= 500){
-    btnTop.style.display = 'block'
-  } else {
-    btnTop.style.display = 'none'
-  }
-})
-btnTop.addEventListener('click',() => {
-  window.scrollTo(0,0)
-})
-
-
-
